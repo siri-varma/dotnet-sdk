4e04a0f7d68383e26d29c4ed89d7051ac0e42202
Siri Varma Vegiraju <siri.varma@outlook.com>
2025-05-16 22:59:28 -0700

    Update Startup.cs
    Signed-off-by: Siri Varma Vegiraju <siri.varma@outlook.com>

--- test/Dapr.Actors.AspNetCore.IntegrationTest.App/Startup.cs
+++ test/Dapr.Actors.AspNetCore.IntegrationTest.App/Startup.cs
@@ -15,7 +15,6 @@ using Dapr.Actors.AspNetCore.IntegrationTest.App.ActivationTests;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace Dapr.Actors.AspNetCore.IntegrationTest.App
 {
